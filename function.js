6fe2dd4b0c5f84f785d33de53c5659d66976e922
Colin Walker <colinwalker270@gmail.com>
2019-08-12 23:12:02 +0000

    feat: Implement deployment to cloud functions
    * Implement a logging hook for deploying to cloud functions.
    * Setup a zip dist to a gs bucket for deploys.
    * Modify deploy yaml file to point to cloud functions exec.

--- function.js
+++ function.js
@@ -2,6 +2,7 @@
 const appFn = require('./lib')
 const { createProbot } = require('probot')
 const { findPrivateKey } = require('probot/lib/private-key')
+const { LoggingBunyan } = require('@google-cloud/logging-bunyan');
 
 const probot = createProbot({
   id: process.env.APP_ID,
@@ -9,5 +10,10 @@ const probot = createProbot({
   cert: findPrivateKey(),
 });
 probot.load(appFn);
+
+// Write logs to stackdriver logging from bunyan logs.
+probot.logger.addStream(
+  new LoggingBunyan().stream(process.env.LOG_LEVEL || 'info'),
+)
 
 module.exports.deliverybot = probot.server;
