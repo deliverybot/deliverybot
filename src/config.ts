1aa2720fd92d98a9355fccb09065a9bef1014a23
Colin Walker <colinwalker270@gmail.com>
2019-08-19 06:15:22 +0000

    chore: Remove uneeeded config

--- src/config.ts
+++ src/config.ts
@@ -1,49 +1,16 @@
 /**
- * -- Required
  * APP_ID
  * PRIVATE_KEY
  * WEBHOOK_SECRET
  * CLIENT_ID
  * CLIENT_SECRET
  * BASE_URL
- *
- * -- Optional
  * LOG_LEVEL
- * EXEC_CLIENT="gcp-build|memory"
- * SECRET_CLIENT="gcp-storage|memory"
  * NODE_ENV="production"
  */
-
-export const SecretClients = {
-  GCPStorage: "gcp-storage",
-  Memory: "memory"
-};
-
-export const ExecClients = {
-  GCPBuild: "gcp-build",
-  None: "none"
-};
 
 export const CLIENT_ID = process.env.CLIENT_ID;
 export const CLIENT_SECRET = process.env.CLIENT_SECRET;
 export const PRODUCTION = process.env.NODE_ENV === "production";
 export const APP_SECRET = process.env.WEBHOOK_SECRET || "development";
 export const BASE_URL = process.env.BASE_URL;
-
-export const SECRET_CLIENT = process.env.SECRET_CLIENT
-  ? process.env.SECRET_CLIENT
-  : PRODUCTION
-  ? SecretClients.GCPStorage
-  : SecretClients.Memory;
-export const EXEC_CLIENT = process.env.EXEC_CLIENT
-  ? process.env.EXEC_CLIENT
-  : PRODUCTION
-  ? ExecClients.GCPBuild
-  : ExecClients.None;
-
-export const EXECUTOR_WHITELIST = process.env.EXECUTOR_WHITELIST === "true";
-export const EXECUTORS: { [k: string]: string | undefined } = {
-  "cloud-functions": "gcr.io/deliverybot/cloud-functions:latest",
-  "cloud-run": "gcr.io/deliverybot/cloud-run:latest",
-  helm: "gcr.io/deliverybot/helm:latest"
-};
