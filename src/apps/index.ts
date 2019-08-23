16f57cc53a70606d02b92fd3adb7c19d04bda097
Colin Walker <colinwalker270@gmail.com>
2019-08-23 01:55:24 +0000

    fix: Remove pubsub app
    Not needed anymore.

--- src/apps/index.ts
+++ src/apps/index.ts
@@ -2,7 +2,6 @@ import { auth } from "./auth";
 import { deploy } from "./deploy";
 import { home } from "./home";
 import { commands } from "./commands";
-import { pubsub } from "./pubsub";
 
 export const apps = [auth, deploy, home];
-export const handlers = [commands, pubsub];
+export const handlers = [commands];
